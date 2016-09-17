c1033f6e7436bbb0c77c70c39e2fd84abd327bf9
wangchong6808 <wangchong6808@163.com>
2016-09-17 21:04:24 +0800

    add more

---
+++ src/main/java/com/spring/boot/practice/MySpringBootAppApplication.java
@@ -0,0 +1,15 @@
+package com.spring.boot.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MySpringBootAppApplication.class, args);
+	}
+
+
+}
