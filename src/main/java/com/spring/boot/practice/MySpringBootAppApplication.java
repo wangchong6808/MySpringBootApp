2954a5d5380b678e0c401ed6012463065889be3b
wangchong6808 <wangchong6808@163.com>
2016-09-18 14:29:58 +0800

    commit changes

--- src/main/java/com/spring/boot/practice/MySpringBootAppApplication.java
+++ src/main/java/com/spring/boot/practice/MySpringBootAppApplication.java
@@ -1,14 +1,27 @@
 package com.spring.boot.practice;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class MySpringBootAppApplication {
 
-	public static void main(String[] args) {
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
-		SpringApplication.run(MySpringBootAppApplication.class, args);
+	public static void main(String[] args) throws Exception {
+		SpringApplication app = new SpringApplication(MySpringBootAppApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.addListeners((ApplicationEvent event) -> {
+			System.out.println("event :::::::::::::"+event.toString());
+		});
+		app.run(args);
+		System.out.println("app type"+app.getMainApplicationClass());
 	}
 
 
