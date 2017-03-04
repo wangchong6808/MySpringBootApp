f43d035caca6b79337f298529888617b5dfde711
ChongWang <wangchong6808@163.com>
2017-03-04 12:23:00 +0800

    commit java file

--- src/main/java/com/spring/boot/practice/Test1.java
+++ src/main/java/com/spring/boot/practice/Test1.java
@@ -17,5 +17,6 @@ public class Test1 {
         calendar.set(Calendar.MINUTE,59);
         calendar.set(Calendar.SECOND,59);
         System.out.println(calendar.getTime().getTime());
+        System.out.println(1);
     }
 }
