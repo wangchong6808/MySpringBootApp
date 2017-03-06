8e5826ae7b403d60acbb3866b4906d751b371483
ChongWang <wangchong6808@163.com>
2017-03-06 20:47:54 +0800

    add 1

--- src/main/java/com/spring/boot/practice/Test1.java
+++ src/main/java/com/spring/boot/practice/Test1.java
@@ -21,6 +21,6 @@ public class Test1 {
         System.out.println(2);
         System.out.println(4);
         System.out.println(3);
-        System.out.println(5);
+        System.out.println(6);
     }
 }
